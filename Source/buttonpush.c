9b0ee808e0010b90f193302eccc257877cf1d327
Eg0rS <71844476+Eg0rS@users.noreply.github.com>
2021-09-26 12:54:53 +0300

    Update buttonpush.c

--- Source/buttonpush.c
+++ Source/buttonpush.c
@@ -2,51 +2,51 @@
 #include "mygpio.h"
 #include "ssec.h"
 
-// Переменные функции записи данных
+// ГЏГҐГ°ГҐГ¬ГҐГ­Г­Г»ГҐ ГґГіГ­ГЄГ¶ГЁГЁ Г§Г ГЇГЁГ±ГЁ Г¤Г Г­Г­Г»Гµ
 int16_t pnum;
-//extern int16_t pnum;  // Число записанных элементов
+//extern int16_t pnum;  // Г—ГЁГ±Г«Г® Г§Г ГЇГЁГ±Г Г­Г­Г»Гµ ГЅГ«ГҐГ¬ГҐГ­ГІГ®Гў
 int16_t pnum1;
-int16_t onb=0;   // Флаг нажатой кнопки
-int16_t starton=0; // Флаг отжатой кнопки
-// Измерение времени паузы и нажатия
+int16_t onb=0;   // Г”Г«Г ГЈ Г­Г Г¦Г ГІГ®Г© ГЄГ­Г®ГЇГЄГЁ
+int16_t starton=0; // Г”Г«Г ГЈ Г®ГІГ¦Г ГІГ®Г© ГЄГ­Г®ГЇГЄГЁ
+// Г€Г§Г¬ГҐГ°ГҐГ­ГЁГҐ ГўГ°ГҐГ¬ГҐГ­ГЁ ГЇГ ГіГ§Г» ГЁ Г­Г Г¦Г ГІГЁГї
 uint32_t ontime1=0;
 uint32_t ontime2=0;
 
 //function for mass and push
-// Возвращает ноль, если ничего не произошло, и единицу, если нужно проанализировать массив
-uint8_t buttonpush() {
+// Г‚Г®Г§ГўГ°Г Г№Г ГҐГІ Г­Г®Г«Гј, ГҐГ±Г«ГЁ Г­ГЁГ·ГҐГЈГ® Г­ГҐ ГЇГ°Г®ГЁГ§Г®ГёГ«Г®, ГЁ ГҐГ¤ГЁГ­ГЁГ¶Гі, ГҐГ±Г«ГЁ Г­ГіГ¦Г­Г® ГЇГ°Г®Г Г­Г Г«ГЁГ§ГЁГ°Г®ГўГ ГІГј Г¬Г Г±Г±ГЁГў
+uint8_t buttonpush() {  // РґРѕР±Р°РІР»СЏРµС‚ РѕС‡РµСЂРµРґРЅРѕРµ РЅР°Р¶Р°С‚РёРµ РЅР° РєРЅРѕРїРєСѓ РІ РјР°СЃСЃРёРІ РЅР°Р¶Р°С‚РёР№
 	uint8_t rezt=0;
 	if (!GGetPin(BUTTON)) {
-    onb=1;
-    if (starton==0) {  // Сюда попадаем, если кнопку только что нажали				
-      ontime1=getssec();
-      starton=1;
-      if(tdlt(ontime2)<20) return rezt; // Исключаем дребезг
-      if (pnum){
-        push[pnum]=tdlt(ontime2);
-        pnum++;
-      }
-	  }
-  } else {
-    if (onb==1) { //Сюда попадаем, если кнопку только что отпустили 
-      onb=0;
-      if(pnum>=MAXIMPULS){
-        pnum=0;
-      } else{
-        push[pnum]=tdlt(ontime1);
-        pnum++;
-      }
-      starton=0;
-      ontime2=getssec();
-    } else {
-// Сюда попадаем, если кнопку уже давно отпустили
-      if (ontime2&&(tdlt(ontime2)>1000)) {
-        rezt=1;
-        ontime2=0;
+    	onb=1;
+    	if (starton==0) {  			
+      		ontime1=getssec();
+      		starton=1;
+      		if(tdlt(ontime2)<20) return rezt; 
+      			if (pnum){
+        			push[pnum]=tdlt(ontime2);
+        			pnum++;
+      			}
+	  	}
+  	} else {
+    		if (onb==1) { 
+      			onb=0;
+      			if(pnum>=MAXIMPULS){
+        			pnum=0;
+      			} else{
+        			push[pnum]=tdlt(ontime1);
+        			pnum++;
+      			}
+      			starton=0;
+      			ontime2=getssec();
+    		} else {
+
+      			if (ontime2&&(tdlt(ontime2)>1000)) {
+        			rezt=1;
+        			ontime2=0;
 				pnum1 = pnum;
-        pnum=0;
-      }
-    }
-  }
+        			pnum=0;
+      			}
+    		}
+  	}
 	return rezt;
 }
