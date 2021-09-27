b276590bb9cd5d57c760cee5ab43b3eee91f115a
Eg0rS <71844476+Eg0rS@users.noreply.github.com>
2021-09-27 22:43:19 +0300

    Update buttonpush.c

--- Source/buttonpush.c
+++ Source/buttonpush.c
@@ -9,7 +9,6 @@ int16_t starton = 0;
 uint32_t ontime1 = 0;
 uint32_t ontime2 = 0;
 
-
 uint8_t buttonpush() {  // добавляет очередное нажатие на кнопку в массив нажатий
 	uint8_t rezt = 0;
 	if (!GGetPin(BUTTON)) {
