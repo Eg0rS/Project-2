6c7aa7f9693b69a014d0c0deffad836d9ce2a8e7
Eg0rS <71844476+Eg0rS@users.noreply.github.com>
2021-09-27 22:13:47 +0300

    Update playsound.c

--- Source/playsound.c
+++ Source/playsound.c
@@ -1,26 +1,28 @@
 #include "playsound.h"
 #include "ssec.h"
 
-int16_t action_time=0;
-int16_t pnum2=0; 
-int16_t playsound=0;
+int16_t action_time = 0;
+int16_t pnum2 = 0; 
+int16_t playsound = 0;
 
-// Запускает проигрывание звука
 void StartSound(void) {
-  pnum2=0;
-  playsound=1;
-  action_time=getssec();
+  pnum2 = 0;
+  playsound = 1;
+  action_time = getssec();
   Sound(1); 
 }
 
-// Функция проигрывания массива
 void beep(void){
 	if (playsound) {
-		if (tdlt(action_time)>=push[pnum2]) {
+		if (tdlt(action_time) >= push[pnum2]) {
 			pnum2++;
-			if (pnum2<pnum1) {
+			if (pnum2 < pnum1) {
 				action_time=getssec();
-				if (pnum2&1) Sound(0); else Sound(1);
+				if (pnum2 & 1){ 
+					Sound(0);
+				} else { 
+					Sound(1);
+				}
 			} else {
 				playsound=0;
 				Sound(0);
