a08b7e64c671a6a1211b78f672a9dddc3ab6e588
Eg0rS <71844476+Eg0rS@users.noreply.github.com>
2021-10-03 12:50:55 +0300

    Update manualkey.c

--- Source/manualkey.c
+++ Source/manualkey.c
@@ -4,26 +4,30 @@
 #include "lockproc.h"
 
 void manualkey(void) {
-	static uint32_t opentime=0;
-	static uint32_t closetime=0;
-	if (GGetPin(KEYOPEN)) {
-		if (opentime) {
-			if (tdlt(opentime)>30) {
-				DoorCmd(1);
+	static uint32_t opentime=0;			//Время нажатия на кнопку открытия. 0 - ничего не делаем. отличное от 0 - обрабатываем нажатие.
+	static uint32_t closetime=0;			//Время нажатия на кнопку закрытия. 0 - ничего не делаем. отличное от 0 - обрабатываем нажатие.
+	if (GGetPin(KEYOPEN)) {				//Если кнопка не нажата.
+		if (opentime) {				//Если время нажатия не равно 0.
+			if (tdlt(opentime)>30) {	//Если время нажатия больше 30мс. (отсекание дребезга)
+				DoorCmd(1);		//Открытие двери.
 			}
-		  opentime=0;
-	  }
-	} else {
-		if (!opentime) opentime=getssec();
+		  opentime=0;				
+	  	}
+	} else {					//Если кнопка нажата.
+		if (!opentime){ 			//Если кнопка не была нажата
+			opentime=getssec();		//Засекаем время нажатия.
+		}
 	}
-	if (GGetPin(KEYCLOSE)) {
-		if (closetime) {
-			if (tdlt(closetime)>30) {
-				DoorCmd(0);
+	if (GGetPin(KEYCLOSE)) {			//Если кнопка не нажата.
+		if (closetime) {			//Если время нажатия не равно 0.
+			if (tdlt(closetime)>30) {	//Если время нажатия больше 30мс. (отсекание дребезга)
+				DoorCmd(0);		//Закрытие двери.
 			}
-		  closetime=0;
-	  }
-	} else {
-		if (!closetime) closetime=getssec();
+		closetime=0;
+	  	}
+	} else {					//Если кнопка нажата.
+		if (!closetime){			//Если кнопка не была нажата
+			closetime=getssec();		//Засекаем время нажатия.
+		}
 	}
 }
